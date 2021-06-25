7645fb75c2e863563d2ea0f109b84a58e588c4e7
Gezal Bahmani <gezalb@berkeley.edu>
2021-06-25 15:44:03 -0700

    triangle

--- lab03/TriangleDrawer.java
+++ lab03/TriangleDrawer.java
@@ -3,13 +3,13 @@ import java.io.*;
 public class TriangleDrawer {
 
     public static void main(String[] args) {
-        int row = 1;
+        int row = 0;
         int SIZE = 10;
         while (row < SIZE) {
             row = row +1;
             int col = 0;
             while (col< row) {
-                System.out.print("*");
+                System.out.print('*');
                 col = col+1;
             }
             System.out.println();
