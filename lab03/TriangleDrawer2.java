33b95f17cb57f6c4aee51de8900837b691295f19
Gezal Bahmani <gezalb@berkeley.edu>
2021-06-25 16:02:05 -0700

    tri2

---
+++ lab03/TriangleDrawer2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class TriangleDrawer2 {
+
+    public static void main(String[] args) {
+        int temp = 1;
+        for (int row = 0; row < 10; row+=1) {
+            for (int col =0; col < temp; col++) {
+                  System.out.println('*');
+            }
+            temp = temp+1;
+            System.out.println();
+        }
+
+    }
+}
